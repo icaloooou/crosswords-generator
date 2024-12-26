6c64fdd97e6d43fe8a994bcf1de0485ff9057b13
icaloooou <rockingrid6@gmail.com>
2024-12-26 19:36:26 -0300

    (feat) v2 - save grids into PDF files

--- README.md
+++ README.md
@@ -13,15 +13,21 @@
 * `verify_word`: Primeiro verifica se é possivel inserir a palavra dentro da matriz (validação de tamanho), em seguida verificamos se já existe alguma letra nesse espaço onde a palavra se encaixa.
 * `insert_word`: Faz a inserção da palavra letra a letra dentro dos espaços da matriz.
 * `insert_random_letters`: Após inserir todas as palavras dentro da matriz, é feito a inserção de letras aleatórias dentro dos espaços que ficaram vazios.
-* `print_grid`: Nesse caso estamos printando o resultado do grid com as palavras inseridas e as letras aleatórias.
-* `make_crosswords`: Função responsável por organizar e chamar as demais funções.
+* `config_pdf`: Nessa função fazemos a configuração do nosso grid.
+* `config_words`: Assim como configuramos o grid, nós também estilizamos as palavras que estão escondidas dentro do grid.
+* `save_grid_18`: Configuramos para poder salvar em PDF os grids de 18x18.
+* `save_grid_9`: Configuramos para poder salvar em PDF os grids de 9x18.
+* `make_crosswords`: Função responsável por organizar e chamar as demais funções para formar o caça-palavras.
+* `game`: Essa função organiza a chamada de todas as outras funções acima, gerando nossos caç-palavras.
 
 ## Implementar :thought_balloon:
 1. Leitura das palavras em um banco de dados, ao invés de uma lista manual;
-2. Salvamento dos resultados em arquivos, ao invés de printar em tela;
-3. ~~Geração de matrizes com dois tamanhos para criação de retangulos, ao invés de sempre ser uma matriz quadrada.~~
+2. ~~Salvamento dos resultados em arquivos, ao invés de printar em tela;~~
+3. ~~Geração de matrizes com dois tamanhos para criação de retangulos, ao invés de sempre ser uma matriz quadrada;~~
+4. Geração de board com streamlit para podermos "rodar" nosso gerador de caça-palavras de forma mais amigável.
 
 ## Tecnologias e bibliotecas
 - `Python`
     - `random`
     - `string`
+    - `reportlab`
--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 
 import random
 import string
+from reportlab.lib.pagesizes import A5
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
+from reportlab.lib import colors
 
 LIFETIME = 3
+SIZE_ROW_LIST = [18, 9]
+SIZE_COLUMN = 18
+REPETITIONS_ROWS = {9: 1, 18: 3}
 
 
 def make_grid(size_r, size_c):
@@ -48,37 +54,82 @@ def insert_random_letters(grid):
                 grid[i][j] = random.choice(string.ascii_uppercase)
 
 
-def print_grid(grid):
-    for linha in grid:
-        print(' '.join(linha))
+def config_pdf(table):
+    table.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, -1), colors.lightgrey),
+                        ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+                        ('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
+                        ('ROUNDEDCORNERS', [5, 5, 5, 5]),
+                        ('BOX',(0,0),(-1,-1),2, colors.black),
+                        ('VALIGN',(0,-1),(-1,-1), 'MIDDLE')]))
+    return table
 
- 
-def make_crosswords(words):  
-    size_row_list = [18, 9]
-    size_column = 18
-    repetition_rows = {9: 2, 18: 3}
 
-    i = 0
-    while i < LIFETIME:
-        for r in size_row_list:
-            for _ in range(repetition_rows[r]):
-                size_row = r
+def config_words(words):
+    words.setStyle(TableStyle([('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
+                        ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+                        ('ROUNDEDCORNERS', [5, 5, 5, 5]),
+                        ('BOX',(0,0),(-1,-1),2, colors.black)]))
+    return words
 
-                grid = make_grid(size_row, size_column)
-                directions = ['diagonal', 'vertical', 'horizontal']
-                for word in words:
-                    put = False
-                    while not put:
-                        row = random.randint(0, size_row - 1)
-                        column = random.randint(0, size_column - 1)
-                        direction = random.choice(directions)
-                        if verify_word(grid, word, row, column, direction):
-                            insert_word(grid, word, row, column, direction)
-                            put = True
-                insert_random_letters(grid)
-                print_grid(grid)
-                i += 1
+
+def save_grid_18(grid, num_life, num_game, words):
+    pdf_name = ".\Jogo_{}_{}.pdf".format(num_game, num_life)
+    pdf = SimpleDocTemplate(pdf_name, pagesize=A5)
+    t = Table(grid)
+    table = config_pdf(t)
+    w = Table([words])
+    word_list = config_words(w)
+
+    elements = [table, Spacer(1, 30), word_list]
+    pdf.build(elements)
+
+def save_grid_9(grid_one, grid_two, num_life, num_game, words):
+    pdf_name = ".\Jogo_{}_{}.pdf".format(num_game, num_life)
+    pdf = SimpleDocTemplate(pdf_name, pagesize=A5)
+    
+    t_one = Table(grid_one)
+    table_one = config_pdf(t_one)
+    t_two = Table(grid_two)
+    table_two = config_pdf(t_two)
+    w = Table([words])
+    word_list = config_words(w)
+
+    elements = [table_one, Spacer(1, 20), word_list, Spacer(1, 10), table_two, Spacer(1, 20), word_list]
+    pdf.build(elements)
+
+
+def make_crosswords(words, size_row, size_column):
+    grid = make_grid(size_row, size_column)
+    directions = ['diagonal', 'vertical', 'horizontal']
+    for word in words:
+        put = False
+        while not put:
+            row = random.randint(0, size_row - 1)
+            column = random.randint(0, size_column - 1)
+            direction = random.choice(directions)
+            if verify_word(grid, word, row, column, direction):
+                insert_word(grid, word, row, column, direction)
+                put = True
+    insert_random_letters(grid)
+    return grid
+
+
+def game(words):
+    life = 0
+    while life < LIFETIME:
+        for size_row in SIZE_ROW_LIST:
+            for game in range(REPETITIONS_ROWS[size_row]):
+                if size_row == 9:
+                    grid_one = make_crosswords(words, size_row, SIZE_COLUMN)
+                    grid_two = make_crosswords(words, size_row, SIZE_COLUMN)
+                    num_game = game + size_row
+                    save_grid_9(grid_one, grid_two, life, num_game, words)
+                else:
+                    grid = make_crosswords(words, size_row, SIZE_COLUMN)
+                    num_game = game + size_row
+                    save_grid_18(grid, life, num_game, words)
+        life += 1
 
 if __name__ == '__main__':
     words = ["PYTHON", "CHALLENGE", "ALGORITHM", "CROSSWORD", "SEARCH"]
-    make_crosswords(words)
+    game(words)
